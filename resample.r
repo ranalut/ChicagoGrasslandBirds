30cbcd77e7fb66e75c527611fa5f6a75d43797f5
ranalut <chadwilsey@gmail.com>
2013-12-30 12:01:41 -0800

    Works to output tif files.

--- resample.r
+++ resample.r
@@ -22,11 +22,23 @@ for (i in 1:length(nass.files))
 
 all.nass.30m <- stack(all.rasters)
 
-writeRaster(all.nass.30m, paste(workspace,'all_nass_clip_30m.nc',sep=''),varname='cdl', zname='years', zunit='year', overwrite=TRUE)
+# writeRaster(all.nass.30m, paste(workspace,'all_nass_clip_30m.nc',sep=''),varname='cdl', zname='years', zunit='year', overwrite=TRUE) # This works based on test below, but crashes.  Likely b/c the files are just too big as netcdf files.  Tifs are smaller.
 
-# test <- resample(x=raster(paste(workspace,'NASS_layers/',nass$NASS[4],sep='')),y=ref.grid,method='ngb',filename=paste(workspace,'NASS_layers/2009_nass_clip_30m.tif',sep=''))
+# =================================================================
+# Test raster stack idea
+# r1 <- raster(matrix(seq(1,9,1),ncol=3))
+# r2 <- r1 + 10
+# r3 <- r1 + 20
+# rs <- stack(r1,r2,r3)
+# writeRaster(rs, paste(workspace,'test.nc',sep=''),varname='cdl', zname='years', zunit='year', overwrite=TRUE) # This appears to work.  Just not with the large NAS files above.
+# =================================================================
 
-# stop('cbw')
+
+
+
+
+
+
 
 
 
