8b0c98de10b7cf67d9c8a56a1deed3bdf3b5a491
ranalut <chadwilsey@gmail.com>
2013-12-30 13:11:21 -0800

    focal.proportions.r: error with NAs

--- focal.proportions.r
+++ focal.proportions.r
@@ -3,9 +3,14 @@ library(ncdf)
 
 prop.of.window <- function(x,value)
 {
+	ifelse(sum(is.na(x))>0,return(NA),x <- x[x!=0])
+	print(x)
 	output <- x[x==value] / length(x) # Think about what the output of focalWeight is.  Does in include all numbers in a square with corners as zero when you select type='circle'?
 	return(output)
 }
+
+workspace <- 'D:/Chicago_Grasslands/GIS/nass_layers/'
+writeRaster(all.rasters[[i]],paste(workspace,(2013-i),'_nass_clip_30m.tif',sep=''),overwrite=TRUE)
 
 nass<-read.csv('d:/chicago_grasslands/gis/nass_file_lists.csv',header=T,stringsAsFactors=FALSE)
 variables <- colnames(nass)[-1]
@@ -20,7 +25,7 @@ for (i in 1:dim(all.nass.30m)[3])
 	
 	for (j in 1:length(variables))
 	{
-		focal.prop[[i]] <- focal(all.nass.30m[[1]], w=focalweight(x=all.nass.30m[[1]], d=250, type='circle'), fun=prop.of.window, value=values[j])
+		focal.prop[[i]] <- focal(all.nass.30m[[1]], w=focalWeight(x=all.nass.30m[[1]], d=250, type='circle'), fun=prop.of.window, value=values[j])
 		writeRaster(focal.prop[[i]], paste(workspace,variables[j],'nass_clip_30m.tif',sep=''),overwrite=TRUE)
 		stop('cbw')
 	}
