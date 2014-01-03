d6ade6c4f2df0b5ae9a2f217b12058943d9bf3e6
ranalut <chadwilsey@gmail.com>
2014-01-02 16:30:36 -0800

    focal.proportions.r updated, remove temp files

--- focal.proportions.r
+++ focal.proportions.r
@@ -20,23 +20,31 @@ for (i in 1:length(years))
 	r.temp <- raster(paste(workspace,(2013-i),'_nass_clip_30m.tif',sep=''))
 	# r.temp <- crop(x=r.temp,y=extent(600000,650000,2050000,2100000)) # For testing/debugging
 	# plot(r.temp)
-	is.na(r.temp) <- 0
+	is.na(r.temp) <- 0 # Creates a temporary file
 	
 	for (j in 1:length(variables))
 	{
+		if (file.exists(paste(workspace,years[i],'_',variables[j],'_nass_30m_r',radius,'.tif',sep=''))==TRUE) 
+		{
+			cat('year',years[i],'variable',variables[j],Sys.time()-startTime,'\n')
+			next(j)
+		}
+		
 		# Rescore to binary raster
 		r.binary <- r.temp
-		r.binary[r.binary!=values[j]] <- 0
-		r.binary[r.binary==values[j]] <- 1
+		r.binary[r.binary!=values[j]] <- 0 # Creates a temporary file
+		r.binary[r.binary==values[j]] <- 1 # Creates a temporary file
 		# plot(r.binary)
-		w.matrix <- focalWeight(x=raster.temp, d=radius, type='circle')
+		w.matrix <- focalWeight(x=r.temp, d=radius, type='circle')
 		focal.prop <- focal(r.binary, w=w.matrix) # , pad=TRUE, padValue=0)
 		# print(focal.prop)
 		# plot(focal.prop, main=paste(years[i],variables[j]))
 		writeRaster(focal.prop, paste(workspace,years[i],'_',variables[j],'_nass_30m_r',radius,'.tif',sep=''),overwrite=TRUE)
+		cat('year',years[i],'variable',variables[j],Sys.time()-startTime,'\n')
 		# stop('cbw')
-		cat('year',years[i],'variable',variables[j],startTime-Sys.time(),'\n')
 	}
+	
+	# Remove all the temporary files for that year's calculations.  
+	file.remove(dir('c:/users/cwilsey/appdata/local/temp/r_raster_tmp/cwilsey',full.names=TRUE))
 	# stop('cbw')
 }
-
