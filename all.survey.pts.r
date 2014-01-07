94d0fa23e9887014e3b7169c3c632961142e2a6a
ranalut <chadwilsey@gmail.com>
2014-01-07 12:49:10 -0800

    all.survey.pts.r, not using this, ArcGIS faster

--- all.survey.pts.r
+++ all.survey.pts.r
@@ -1,45 +1,43 @@
 
-# This version didn't work.  I wasn't able to export a shapefile created in the sp package so that it lined up with the same points in the ESRI shapefile.  This is something I've struggled with before and will need to figure out eventually.
-
-# Instead, I removed duplicates with the 'delete identical' command in ArcGIS.  This left me with 2117 points.  I then reprojected that file from WGS84 to Albers Equal Area to match the CDL tiffs.  Will work with that points file below...
-
 library(rgdal)
-library(raster)
 library(sp)
 
-# all.data <- read.csv("D:/Chicago_Grasslands/BIRD_DATA/BCN/31qryBreeding_JGS_version.csv", header=TRUE)
-
-unique.pts <- readOGR(dsn='D:/Chicago_Grasslands/BIRD_DATA/BCN',layer='unique_pts_albers',encoding='ESRI Shapefile')
-print(unique.pts)
+all.pts <- readOGR(dsn='D:/Chicago_Grasslands/BIRD_DATA/BCN',layer='all_records_jgs_version',encoding='ESRI Shapefile')
+print(all.pts) # This works.
 
 # trim observations to those meeting requirements
-unique.pts <- all.data[unique.pts$VALID==1 & 
-				unique.pts$PROTOCOL_ID=="P21" & 
-				unique.pts$DURATION_HRS==0.08 &
-				unique.pts$ALL_OBS_REPORTED==1,]
+obs <- all.pts[all.pts$VALID==1 & 
+				all.pts$PROTOCOL_I=="P21" & 
+				all.pts$DURATION_H==0.08 &
+				all.pts$ALL_OBS_RE==1,]
 
+print(obs) # This works.  92898 records.
+
+# test <- obs[1:10,]
+# writeOGR(test,'D:/Chicago_Grasslands/BIRD_DATA/BCN','test_pts',driver='ESRI Shapefile') # This looks good.
+
+unique.pts <- remove.duplicates(obs) # This takes a long time (30+ minutes).  ArcGIS is faster.
 print(unique.pts)
-stop('cbw')
+writeOGR(unique.pts,'D:/Chicago_Grasslands/BIRD_DATA/BCN','unique_pts_wgs84',driver='ESRI Shapefile')
+
+# ===================================================
+# Will now reproject unique data points into an Albers equal area projection in ArcGIS.
+
+
+
+
+# ===================================================
+# Unused code
 
 # Create unique list of count submissions per birder (SUB_ID).  The VALID field lists the number of species submitted for each unique count.  The output includes repeat (annual or more frequent) counts at the same point.
 # counts <- aggregate(VALID ~ SUB_ID + JHOUR + JDATE + YEAR + LATITUDE + LONGITUDE, obs, length)
 # hist(counts$YEAR, xlab="year", ylab="frequency", main="5-minute point counts by year")
 # hist(counts$JDATE, xlab="julian day", ylab="frequency", main="5-minute point counts by day of year")
 
-obs.pts <- SpatialPointsDataFrame(obs,coords=obs[,c('LATITUDE','LONGITUDE')],proj4string=CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'))
-test <- obs.pts[1:10,]
-writeOGR(obs.pts,'D:/Chicago_Grasslands/BIRD_DATA/BCN','test_unique_pts',driver='ESRI Shapefile') # ,overwrite_layer=TRUE)
-stop('cbw')
-
+# Proj4 projection text
 # +proj=longlat +ellps=WGS84 +no_defs
 # +proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs
 # +proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0
 # +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs
 
-print(dim(obs.pts))
-obs.pts <- remove.duplicates(obs.pts) # This takes a long time (30+ minutes)
-print(dim(obs.pts))
 
-writeOGR(obs.pts,'D:/Chicago_Grasslands/BIRD_DATA/BCN','unique_pts2',driver='ESRI Shapefile') # ,overwrite_layer=TRUE)
-
-stop('cbw')
