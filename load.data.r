67a0c445044944c2c24d21ed7a189ec512dcfd53
ranalut <chadwilsey@gmail.com>
2014-01-08 09:17:23 -0800

    load.data.r update

--- load.data.r
+++ load.data.r
@@ -1,41 +1,59 @@
 
-# Will create raster stacks for all variables (multiple spatial scales) within the same year. Then, will sample all stacks with the points.  Then, will select which year's data stack to use based on individual points and records.
+library(rgdal)
+library(raster)
 
+# Define inputs and file paths
 years <- c(2007, 2009)
 radius <- c(100, 500)
 days <- c(215,156)
 bands <- seq(1,7,1)
 nass.var <- c('water','herb.wetland','grass.hay','alfalfa.etc','dev.low','dev.high','decid.wood','wood.wetland','other')
 
+nass.path <- 'd:/chicago_grasslands/gis/nass_layers/'
+landsat.path <- 'd:/chicago_grasslands/landsat2/'
+output.path <- 'd:/chicago_grasslands/models/'
+
 nass.data <- list()
 landsat.data <- list()
-point.data <- list()
+
+# Load the unique points for model building
+# Albers equal area
+unique.pts <- readOGR(dsn='D:/Chicago_Grasslands/BIRD_DATA/BCN',layer='unique_pts_albers_v2',encoding='ESRI Shapefile')
+print(unique.pts)
+unique.pts@data <- unique.pts@data[,30:39]
+# UTM zone 16N
+unique.pts.2 <- readOGR(dsn='D:/Chicago_Grasslands/BIRD_DATA/BCN',layer='unique_pts_utm16n_v2',encoding='ESRI Shapefile')
+print(unique.pts.2)
+unique.pts.2@data <- unique.pts.2@data[,30:39]
 
 for (i in 1:length(years))
 {
-	nass.file.names <- c(paste(years[i],'_',nass.var,'_nass_30m_r',radius[1],'.tif',sep=''),paste(years[i],'_',nass.var,'_nass_30m_r',radius[2],'.tif',sep=''))
-	print(nass.file.names)
-	nass.data[[i]] <- stack(nass.file.names)
+	nass.file.names <- c(
+		paste(nass.path,years[i],'_',nass.var,'_nass_30m_r',radius[1],'.tif',sep=''),
+		paste(nass.path,years[i],'_',nass.var,'_nass_30m_r',radius[2],'.tif',sep='')
+		)
+	# print(nass.file.names)
+	temp <- stack(nass.file.names)
+	# print(temp)
+	nass.data[[i]] <- extract(temp,unique.pts)
+	colnames(nass.data[[i]]) <- c(paste(nass.var,'.',radius[1],sep=''),paste(nass.var,'.',radius[2],sep=''))
+	nass.data[[i]] <- data.frame(unique.pts@data,nass.data[[i]])
+	write.csv(nass.data[[i]],paste(output.path,years[i],'.nass.extract.csv',sep=''))
+	# stop('cbw')
 	
-	landsat.file.names <- c(paste(years[i],'_d',days[i],'_b',bands,'_r',radius[1],'.tif',sep=''),paste(years[i],'_d',days[i],'_b',bands,'_r',radius[2],'.tif',sep=''))
-	print(landsat.file.names)
-	landsat.data[[i]] <- stack(landsat.file.names)
+	landsat.file.names <- c(
+		paste(landsat.path,years[i],'_d',days[i],'_b',bands,'_r',radius[1],'.tif',sep=''),
+		paste(landsat.path,years[i],'_d',days[i],'_b',bands,'_r',radius[2],'.tif',sep='')
+		)
+	# print(landsat.file.names)
+	temp2 <- stack(landsat.file.names)
+	# print(temp2)
+	landsat.data[[i]] <- extract(temp2,unique.pts.2)
+	colnames(landsat.data[[i]]) <- c(paste('b',bands,'.',radius[1],sep=''),paste('b',bands,'.',radius[2],sep=''))
+	landsat.data[[i]] <- data.frame(unique.pts.2@data,landsat.data[[i]])
+	write.csv(landsat.data[[i]],paste(output.path,years[i],'.landsat.extract.csv',sep=''))
 	# stop('cbw')
 }
-stop('cbw')
 
-
-
-## select observations for species of interest
-sppname<-paste("graspa")
-spp<-obs[obs$SPECIES_CODE==sppname,]
-
-## join to set of unique submissions
-
-spp_PA<-merge(spp, counts, by=c("SUB_ID", "JHOUR", "JDATE", "LATITUDE", "LONGITUDE"), all=T)
-i<-is.na(spp_PA$HOW_MANY_ATLEAST)
-spp_PA$HOW_MANY_ATLEAST[i]<-0
-spp_PA<-spp_PA[,-(6:9)]
-spp_PA<-spp_PA[,-(7:30)]
-
-write.csv(spp_PA,paste(sppname, "_PA.csv", sep=""), row.names=F)
+save(nass.data,landsat.data,file=paste(output.path,'unique.point.data.v1.rdata',sep=''))
+# stop('cbw')
