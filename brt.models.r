53c9ad04db5b209f67628198485d739bd2e07d64
ranalut <chadwilsey@gmail.com>
2014-01-14 08:15:41 -0800

    brt.models.r update

--- brt.models.r
+++ brt.models.r
@@ -3,14 +3,14 @@
 
 library(dismo)
 library(gbm)
+library(tcltk)
+# library(randomForest)
 # library(pROC)
 # library(ROCR)
 # library(rgdal)
-# library(tcltk)
 # library(mgcv)
 
-setwd('d:/github/chicagograsslandbirds/')
-output.path <- 'd:/chicago_grasslands/models/'
+source('settings.r')
 
 # Step 1 ============================================
 # Creates spatial points objects for the unique survey points in the BCN dataset and extracts data from the nass and landsat datasets, assembled a raster stacks.  Output is saves as .csv files in /Models/ folder and as an R workspace.
@@ -26,17 +26,17 @@ output.path <- 'd:/chicago_grasslands/models/'
 # Now, we will build BRT models for individual species...
 load(file=paste(output.path,'species.data.v1.rdata',sep=''))
 
-spp.names <- c('boboli','sedwre','henspa','easmea','graspa')
-years <- c(2007,2009)
-lr <- c(0.01,0.005,0.01,0.01,0.01)
-
 # NASS models
 nass.models <- list()
 for (i in 1:length(nass.spp.data))
 {
 	cat('\n\n\n\n\nstart nass',spp.names[i],'########################\n')
-	nass.models[[i]] <- gbm.step(data=nass.spp.data[[i]], gbm.x=c(4:5,9:26), gbm.y=6, family="poisson", 
-	tree.complexity=5, learning.rate=lr[i], bag.fraction=0.5)
+	cat('points considered...',dim(nass.spp.data[[i]])[1],'\n')
+	nass.models[[i]] <- gbm.step(data=nass.spp.data[[i]], gbm.x=c(4:5,9:26), gbm.y=6, family="poisson", tree.complexity=5, learning.rate=lr[i], bag.fraction=0.5)
+	# nass.models[[i]] <- randomForest(x=nass.spp.data[[i]][,c(4:5,9:26)], y=nass.spp.data[[i]][,6], importance=TRUE)
+	test <- predict.gbm(newdata=nass.spp.data[[i]][,c(4:5,9:26)], nass.models[[i]], n.trees=nass.models[[i]]$n.trees, type='response', progress='window', na.rm=TRUE)
+	plot(test ~ nass.spp.data[[i]][,6], main=paste(spp.names[i],', fitted ~ obs',sep=''), xlab='counts', ylab='predicted')
+	print(table(round(test)))
 	cat('end nass',spp.names[i],'############################\n')
 	# stop('cbw')
 }
@@ -47,11 +47,19 @@ landsat.models <- list()
 for (i in 1:length(nass.spp.data))
 {
 	cat('\n\n\n\nstart landsat',spp.names[i],'########################\n')
+	cat('points considered...',dim(nass.spp.data[[i]])[1],'\n')
 	landsat.models[[i]] <- gbm.step(data=landsat.spp.data[[i]], gbm.x=c(4:5,9:22), gbm.y=6, family="poisson", tree.complexity=5, learning.rate=lr[i], bag.fraction=0.5)
+	# landsat.models[[i]] <- randomForest(x=landsat.spp.data[[i]][,c(4:5,9:22)], y=landsat.spp.data[[i]][,6], importance=TRUE)
+	test <- predict.gbm(newdata=landsat.spp.data[[i]][,c(4:5,9:22)], landsat.models[[i]], n.trees=landsat.models[[i]]$n.trees, type='response', progress='window', na.rm=TRUE)
+	plot(test ~ landsat.spp.data[[i]][,6], main=paste(spp.names[i],', fitted ~ obs',sep=''), xlab='counts', ylab='predicted')
+	print(table(round(test)))
 	cat('end landsat',spp.names[i],'############################\n')
 	# stop('cbw')
 }
 
+# Version 1 are full models
+# Version 2 were RF models, but I suspect that I wrote over them.
+# Version 3 are models w/o JDATE and JHOUR 
 save(nass.models, landsat.models, file=paste(output.path,'species.models.v1.rdata',sep=''))
 
 # ========================================================================
