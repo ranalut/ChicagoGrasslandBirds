9f2b65aebfb42ce3277beeff08b4fb5cd38412bf
ranalut <chadwilsey@gmail.com>
2014-01-14 12:03:43 -0800

    truncate.r update

--- truncate.r
+++ truncate.r
@@ -9,15 +9,15 @@ for (i in 1:length(spp.names))
 	# NASS
 	temp <- raster(paste(output.path,spp.names[i],'.nass.tif',sep=''))
 	print(temp)
-	min.value <- minValue(temp)
-	if (min.value < 0) { temp[temp < 0] <- 0 }
-	writeRaster(temp,paste(output.path,spp.names[i],'.nass.zero.tif',sep=''), overwrite=TRUE)
-	cat('done',spp.names[i],'nass\n')
+	# min.value <- minValue(temp)
+	# if (min.value < 0) { temp[temp < 0] <- 0 }
+	# writeRaster(temp,paste(output.path,spp.names[i],'.nass.zero.tif',sep=''), overwrite=TRUE)
+	# cat('done',spp.names[i],'nass\n')
 	# Landsat
-	temp <- raster(paste(output.path,spp.names[i],'.landsat.albers.tif',sep=''))
+	temp <- raster(paste(output.path,spp.names[i],'.landsat.tif',sep=''))
 	print(temp)
-	min.value <- minValue(temp)
-	if (min.value < 0) { temp[temp < 0] <- 0 }
-	writeRaster(temp,paste(output.path,spp.names[i],'.landsat.albers.zero.tif',sep=''), overwrite=TRUE)
-	cat('done',spp.names[i],'landsat\n')
+	# min.value <- minValue(temp)
+	# if (min.value < 0) { temp[temp < 0] <- 0 }
+	# writeRaster(temp,paste(output.path,spp.names[i],'.landsat.albers.zero.tif',sep=''), overwrite=TRUE)
+	# cat('done',spp.names[i],'landsat\n')
 }
