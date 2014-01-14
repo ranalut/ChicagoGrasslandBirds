f1ab0980e17a250a87402c6acf7db04c90879aa9
ranalut <chadwilsey@gmail.com>
2014-01-14 12:03:32 -0800

    model.prediction.r update, add rounding

--- model.prediction.r
+++ model.prediction.r
@@ -22,7 +22,7 @@ for (i in 1:length(spp.names))
 	{
 		startTime <- Sys.time()
 		nass.pred[[i]] <- predict(nass.pred.data, nass.models[[i]], n.trees=nass.models[[i]]$n.trees, type='response', progress='window', na.rm=TRUE)
-		# nass.pred[[i]] <- predict(nass.models[[i]], newdata=nass.pred.data, na.action=na.omit) # For RF, didn't work b/c of NAs.
+		nass.pred[[i]] <- round(nass.pred[[i]],3)
 		writeRaster(nass.pred[[i]],paste(output.path,spp.names[i],'.nass.tif',sep=''), overwrite=TRUE)
 		cat('end',spp.names[i],'time',Sys.time()-startTime,'\n')
 		stop('cbw')
@@ -33,16 +33,15 @@ for (i in 1:length(spp.names))
 	{
 		startTime <- Sys.time()
 		landsat.pred[[i]] <- predict(landsat.pred.data, landsat.models[[i]], n.trees=landsat.models[[i]]$n.trees, type='response', progress='window', na.rm=TRUE)
-		# landsat.pred[[i]] <- predict(landsat.models[[i]], newdata=landsat.pred.data, na.action=na.omit) # For RF, didn't work b/c of NAs
-		writeRaster(landsat.pred[[i]],paste(output.path,spp.names[i],'.landsat.rf.tif',sep=''), overwrite=TRUE)
-		# landsat.pred.2[[i]] <- projectRaster(from=landsat.pred[[i]], to=landsat.pred.2[[i]])
-		# writeRaster(landsat.pred.2[[i]],paste(output.path,spp.names[i],'.landsat.albers.tif',sep=''), overwrite=TRUE)
+		landsat.pred[[i]] <- round(landsat.pred[[i]],3)
+		writeRaster(landsat.pred[[i]],paste(output.path,spp.names[i],'.landsat.tif',sep=''), overwrite=TRUE)
 		cat('end',spp.names[i],'time',Sys.time()-startTime,'\n')
 	}
 	# stop('cbw')
 }
-# save(nass.pred, file=paste(output.path,'nass.pred.rdata',sep=''))
-# save(landsat.pred, file=paste(output.path,'landsat.pred.rdata',sep=''))
+
+save(nass.pred, file=paste(output.path,'nass.pred.rdata',sep=''))
+save(landsat.pred, file=paste(output.path,'landsat.pred.rdata',sep=''))
 
 # ================================================================
 # Justin's code
