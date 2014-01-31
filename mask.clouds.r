3f801d0f5686c433616b220be38be4b37d484a76
ranalut <chadwilsey@gmail.com>
2014-01-31 10:44:20 -0800

    update mask.clouds.r It works!

--- mask.clouds.r
+++ mask.clouds.r
@@ -1,12 +1,14 @@
 
 mask.clouds <- function(workspace,file.name,level)
 {
-	clouds <- raster(paste(workspace,file.name,'/clouds.rm.',level,'.',file.name,'.tif',sep=''))
+	clouds <- raster(paste(workspace,file.name,'/clouds.rm.shadows.',level,'.',file.name,'.tif',sep=''))
+	# print(clouds) # ; plot(clouds)
 	
 	for (i in 1:7)
 	{
 		landsat <- raster(paste(workspace,file.name,'/atmos.',file.name,'_b',i,'.tif',sep=''))
-		mask(landsat, clouds, inverse=TRUE, filename=paste(workspace,file.name,'/atmos.clean.',file.name,'_b',i,'.tif',sep=''))
+		# print(landsat); stop('cbw')
+		mask(landsat, clouds, inverse=TRUE, filename=paste(workspace,file.name,'/atmos.clean.',file.name,'_b',i,'.tif',sep=''), overwrite=TRUE)
 	}
 }
 
