1a3ab0ff7cda384d6666e828273ab0f7fc31aa12
ranalut <chadwilsey@gmail.com>
2014-02-04 08:29:55 -0800

    updates landsat.processing.r

--- landsat.processing.r
+++ landsat.processing.r
@@ -10,18 +10,18 @@ source('mask.clouds.r')
 
 do.atmos <- 	'n'
 do.clouds <- 	'n'
-do.clean <- 	'n'
-do.shadows <- 	'n'
+do.clean <- 	'y'
+do.shadows <- 	'y'
 do.mask <- 		'y'
 
 # workspace <- 'D:/Chicago_Grasslands/LANDSAT2/'
 workspace <- 'Z:/Chicago_Grasslands/LANDSAT2/'
-file.names <- c('lt50230312006164pac01','lt50230312007167pac01','lt50230312009156pac02','lt50230312010175EDC00','lt50230312011194pac01')
+file.names <- c('lt50230312006164pac01','lt50230312007167pac01','lt50230312008170pac01','lt50230312009156pac02','lt50230312010175EDC00','lt50230312011194pac01')
 
 cloud.shifts <- c(
 2006,-15,-14, # Looks good.
 2007,-31,-27, # Tuned to study area.
-# 2008,NA,NA,
+2008,-25,-18,
 2009,-23,-21, # Tuned to clouds within the study area.
 2010,-20,-13, # This is tuned to the clouds within the study area near downtown Chicago.  There are a couple of clouds west of downtown that are lower and the offset isn't quite right, but these are only a handful and do not overlap with points.
 2011,-15,-12  # Looks good.
@@ -32,7 +32,7 @@ colnames(shifts) <- c('year','x.shift','y.shift')
 min.cloud <- 56
 
 # for (n in 1:5)
-for (n in 2:5)
+for (n in 3)
 {
 	if (do.atmos=='y')
 	{
@@ -41,8 +41,8 @@ for (n in 2:5)
 	
 	if (do.clouds=='y')
 	{
-		level.seq <- seq(0.0012,0.0008,-0.0001) # default = 0.0014
-		# level.seq <- 0.0011
+		# level.seq <- seq(0.0012,0.0008,-0.0001) # default = 0.0014
+		level.seq <- 0.0009
 
 		for (i in level.seq)
 		{
