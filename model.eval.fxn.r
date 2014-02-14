a691c0a0bc4ad40a0af7b87366a7851e3de44a32
ranalut <chadwilsey@gmail.com>
2014-02-14 08:50:58 -0800

    updates model.eval.fxn.r

--- model.eval.fxn.r
+++ model.eval.fxn.r
@@ -1,7 +1,8 @@
 
 # Model evaluation
 
-model.eval <- function(the.model=nass.models[[i]], covariates=nass.spp.data[[i]][,c(4:5,9:26)], test.rows=test.rows, obs=nass.spp.data[[i]][,6], spp=spp.names[i])
+model.eval <- function(the.model, covariates, test.rows, obs, spp)
+# model.eval <- function(the.model=nass.models[[i]], covariates=nass.spp.data[[i]][,c(4:5,9:26)], test.rows=test.rows, obs=nass.spp.data[[i]][,6], spp=spp.names[i])
 {
 	test <- predict.gbm(newdata=covariates[test.rows,], the.model, n.trees=the.model$n.trees, type='response', progress='window', na.rm=TRUE)
 	plot(test ~ obs[test.rows], main=paste(spp,', fitted ~ obs',sep=''), xlab='counts', ylab='predicted')
