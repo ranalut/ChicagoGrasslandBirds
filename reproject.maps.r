773fec77e7cf58bd0854d829db0096de5435c339
ranalut <chadwilsey@gmail.com>
2014-02-14 08:51:17 -0800

    updates reproject.maps.r

--- reproject.maps.r
+++ reproject.maps.r
@@ -3,15 +3,15 @@ library(rgdal)
 
 source('settings.r')
 
-ref.proj <- raster(paste(output.path,'boboli.nass.tif',sep=''))
+ref.proj <- raster(paste(output.path,'boboli.nass.v6.tif',sep=''))
 print(ref.proj)
 
 for (i in 1:length(spp.names))
 {
-	temp <- raster(paste(output.path,spp.names[i],'.landsat.tif',sep=''))
+	temp <- raster(paste(output.path,spp.names[i],'.landsat.v6.tif',sep=''))
 	print(temp)
 	temp2 <- projectRaster(from=temp, to=ref.proj)
 	print(temp2)
-	writeRaster(temp2,paste(output.path,spp.names[i],'.landsat.albers.tif',sep=''), overwrite=TRUE)
+	writeRaster(temp2,paste(output.path,spp.names[i],'.landsat.v6.albers.tif',sep=''), overwrite=TRUE)
 }
 
