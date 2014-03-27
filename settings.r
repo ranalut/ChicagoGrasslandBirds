de3d4f16b8415a7bf0d5c690cf48e44d6ed3a9ac
ranalut <chadwilsey@gmail.com>
2014-03-27 10:52:03 -0700

    update settings.r

--- settings.r
+++ settings.r
@@ -10,23 +10,25 @@ output.path <- paste(drive,':/chicago_grasslands/models/',sep='')
 
 do.data.proc <- 'n' # See processing settings below.  Remove clouds (landsat.processing.r) before running.
 do.load.data <- 'n'
-do.spp.data <- 	'y'
-do.test.data <- 'n' # Be sure not to overwrite if you turn this on.
+do.spp.data <- 	'n'
+do.test.data <- 'n' # DO NOT OVERWRITE. Change output name below if turned on.
 do.models <- 	'n'
-do.prediction <-'n'
+do.eval <- 		'n'
+do.prediction <-'y'
 do.nass <-		'n'
-do.landsat <-	'n'
+do.landsat <-	'y'
+do.kd <- 		'n'
 
 # Define inputs and file paths
-data.yrs <- c(2007,2008,2009,2010,2011) # 2006 # 2009 # c(2007, 2009)
-days <- c(167,170,156,175,194) # 164 # 156 # c(215,156)
-survey.yrs <- data.yrs # c(1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012) # c(2007,2009)
 radius <- c(100,1000) # c(100,500,1000) # c(100, 1000) # c(100,500)
+data.yrs <- c(2009,2010,2011) # c(2007,2008,2009,2010,2011) # 2006 # 2009 # c(2007, 2009)
+days <- c(156,175,194) # c(167,170,156,175,194) # 164 # 156 # c(215,156)
+survey.yrs <- data.yrs # c(1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012) # c(2007,2009)
 bands <- c(1:5,7) # c(7,5) # seq(1,7,1)
 nass.var <- c('water','herb.wetland','grass.hay','alfalfa.etc','dev.low','dev.high','decid.wood','wood.wetland','other')
 spp.names <- c('boboli','sedwre','henspa','easmea','graspa')
 
-# Processing
+# For single year or single radius processing
 if (do.data.proc=='y')
 {
 	years <- 2008 # c(2006,2007,2009,2010,2011) # c(2007,2009) # seq(2012,2006,-1)
@@ -40,8 +42,8 @@ if (do.data.proc=='y')
 lr <- c(0.1,0.005,0.01,0.01,0.01)
 
 # For prediction
-pred.year <- data.yrs[length(data.yrs)]
-pred.day <- days[length(days)]
+pred.year <- '20YY' # '20XX' # 2010 # data.yrs[length(data.yrs)] '20XX' is based on mean for all years.  '20YY' is based on mean for last 3 years (2009-11).
+pred.day <- 'YYY' # 'YYY' # '175 # days[length(days)]
 
 study.area.1 <- extent(matrix(c(593000,707000,2043000,2199000),ncol=2,byrow=TRUE))
 study.area.2 <- extent(matrix(c(345000,470000,4540000,4720000),ncol=2,byrow=TRUE))
@@ -52,6 +54,7 @@ if (do.data.proc=='y')
 {
 	# radius <- c(1000)
 	# Processing NASS data layers to extract percent cover measures at multiple spatial scales:
+	source('calc.focal.prop.r')
 	source('focal.proportions.r')
 	
 	# Processing Landsat data layers to extract mean band values at multiple spatial scales:
@@ -74,14 +77,15 @@ if (do.load.data=='y')
 	# Version 3 2009, all yrs, radius 100 & 1000
 	# Version 4: 2007:2011, 100, 500 radius, cloudless
 	# Version 5: 2007:2011, 100, 1000 radius, cloudless
-	save(nass.data,landsat.data,file=paste(output.path,'unique.point.data.v5.rdata',sep=''))
+	# Version 6: 2007:2011, 500, 1000 radius, cloudless
+	save(nass.data,landsat.data,file=paste(output.path,'unique.point.data.v6.rdata',sep=''))
 }
 # ===============================================================
 # Extract Species Data 
 # Generate datasets for each individual species pulling data for each survey year.
 if (do.spp.data=='y')
 {
-	load(file=paste(output.path,'unique.point.data.v5.rdata',sep='')) # Check Step 2 for versions.
+	# load(file=paste(output.path,'unique.point.data.v5.rdata',sep='')) # Check Step 2 for versions.
 	
 	source('species.data.r') 
 	
@@ -90,7 +94,8 @@ if (do.spp.data=='y')
 	# Version 3: 2009 data with all survey years, radius 100, 1000
 	# Version 4: 2007:2011, 100, 500 radius, cloudless
 	# Version 5: 2007:2011, 100, 1000 radius, cloudless
-	save(nass.spp.data,landsat.spp.data,nass.rows,landsat.rows,file=paste(output.path,'species.data.v5.rdata',sep='')) 
+	# Version 6: 2007:2011, 500, 1000 radius, cloudless
+	save(nass.spp.data,landsat.spp.data,nass.rows,landsat.rows,file=paste(output.path,'species.data.v6.rdata',sep='')) 
 }
 # ===============================================================
 # Test Data
@@ -105,7 +110,7 @@ if (do.test.data=='y')
 if (do.models=='y')
 {
 	# See above for versions.
-	load(file=paste(output.path,'species.data.v4.rdata',sep=''))
+	load(file=paste(output.path,'species.data.v6.rdata',sep=''))
 	test.rows <- scan(file=paste(output.path,'test.rows.v1.txt',sep=''),what=numeric())
 	
 	source('train.test.data.r')
@@ -116,29 +121,79 @@ if (do.models=='y')
 	# Version 4 based on 2009 data, 100 and 1000 radius
 	# Version 5 2007:2011; 100, 500 radius; cloudless
 	# Version 6 2007:2011; 100, 1000 radius; cloudless
-	if (do.nass=='y') { save(nass.models, nass.dev.exp.cv, nass.dev.exp.test, file=paste(output.path,'nass.species.models.v5.rdata',sep='')); write.csv(data.frame(cv=nass.dev.exp.cv,test=nass.dev.exp.test),paste(output.path,'nass.performance.v5.csv',sep='')) } 
-	if (do.landsat=='y') { save(landsat.models, landsat.dev.exp.cv, landsat.dev.exp.test, file=paste(output.path,'landsat.species.models.v5.rdata',sep='')); write.csv(data.frame(cv=landsat.dev.exp.cv,test=landsat.dev.exp.test),paste(output.path,'landsat.performance.v5.csv',sep='')) } 
+	# Version 7 2007:2011; 500, 1000 radius; cloudless
+	if (do.nass=='y') { save(nass.models, file=paste(output.path,'nass.species.models.v7.rdata',sep='')) } 
+	if (do.landsat=='y') { save(landsat.models, file=paste(output.path,'landsat.species.models.v7.rdata',sep='')) } 
 }
-
+# ================================================================
+if (do.eval=='y')
+{
+	load(file=paste(output.path,'species.data.v4.rdata',sep=''))
+	test.rows <- scan(file=paste(output.path,'test.rows.v1.txt',sep=''),what=numeric())
+	ver <- 5 # Version
+	load(file=paste(output.path,'nass.species.models.v',ver,'.rdata',sep=''))
+	load(file=paste(output.path,'landsat.species.models.v',ver,'.rdata',sep=''))
+	
+	source('deviance.explained.r')
+	source('model.eval.fxn.r')
+	source('model.evaluation.r')
+	
+	eval.table <- data.frame(nass.dev.exp.cv,nass.cor.cv,nass.dev.exp.test,nass.cor.test,landsat.dev.exp.cv,landsat.cor.cv,landsat.dev.exp.test,landsat.cor.test)
+	write.csv(eval.table,paste(output.path,'performance.v',ver,'.csv',sep=''))
+}
 # ================================================================
 # Prediction 
 if (do.prediction=='y')
 {
-	source('load.prediction.data.r')
-	# Version 1 is uncropped, Version 2 is cropped, Version 3 is larger
-	# Version 4 is larger for Landsat, extends further west.
-	save(nass.pred.data, landsat.pred.data, file=paste(output.path,'pred.data.v3.rdata',sep='')) # This may reference temporary files and thus may not really be savable.
-	# stop('cbw')
-
-	# Load models - each object is a list of models, one for each species.
-	# load(file=paste(output.path,'pred.data.v3.rdata',sep='')) # Check version above under Build Models
-	if (do.nass=='y') { load(file=paste(output.path,'nass.species.models.v2.rdata',sep='')) } 
-	if (do.landsat=='y') { load(file=paste(output.path,'landsat.species.models.v2.rdata',sep='')) }
+	radii <- list(c(100,1000)) # list(c(100,500),c(100,1000),c(500,1000))
+	versions <- '6c' # c(5,6,7) # If working with a version #b, need to copy and rename models rdata file.
 	
-	source('model.prediction.r') # Be sure to change the file output names
+	for (tt in 1:length(versions))
+	{
+		radius <- radii[[tt]]
+		ver <- versions[tt]
+		
+		source('load.prediction.data.r')
+		
+		# ver <- 6
+		if (do.nass=='y') { load(file=paste(output.path,'nass.species.models.v',ver,'.rdata',sep='')) } 
+		if (do.landsat=='y') { load(file=paste(output.path,'landsat.species.models.v',ver,'.rdata',sep='')) }
+		
+		source('model.prediction.r') # Be sure to change the file output names
+		
+		# Version 2 is all survey years, 2009 imagery, 100, 500 radius
+		# Version 3 is all survey years, 2009 imagery, 100, 1000 radius
+		# Version 5 2007:2011; 100, 500 radius; cloudless
+		# Version 6 2007:2011; 100, 1000 radius; cloudless
+		# Version 6b is projected landsat with 5-yr average.  6c is 3-yr average.
+		# Version 7 2007:2011; 500, 1000 radius; cloudless
+		if (do.nass=='y') { save(nass.pred, file=paste(output.path,'nass.pred.v',ver,'.rdata',sep='')) }
+		if (do.landsat=='y') { save(landsat.pred, file=paste(output.path,'landsat.pred.v',ver,'.rdata',sep='')) }
+	}
+}
+# ====================================================================
+# Kernel Density
+if (do.kd=='y')
+{
+	the.radius <- 2000
+	load(file=paste(output.path,'species.data.v6.rdata',sep=''))
+	ref.raster <- raster(paste(output.path,'boboli.nass.v7.tif',sep=''))
+	unique.pts <- readOGR(dsn=paste(drive,':/Chicago_Grasslands/BIRD_DATA/BCN',sep=''),layer='unique_pts_albers_v2',encoding='ESRI Shapefile')
+	coords.crosswalk <- data.frame(coordinates(unique.pts),unique.pts@data[,c('LATITUDE', 'LONGITUDE')])
+	colnames(coords.crosswalk) <- c('x.coord','y.coord','LATITUDE','LONGITUDE')
+	# print(head(coords.crosswalk))
 	
-	# Version 2 is all survey years, 2009 imagery, 100, 500 radius
-	# Version 3 is all survey years, 2009 imagery, 100, 1000 radius
-	if (do.nass=='y') { save(nass.pred, file=paste(output.path,'nass.pred.v2.rdata',sep='')) } # All survey yrs, 2009 imagery
-	if (do.landsat=='y') { save(landsat.pred, file=paste(output.path,'landsat.pred.v2.rdata',sep='')) } # All survey yrs, 2009 imagery
+	source('kernel.density.r')
+	
+	for (i in 1:5)
+	{
+		presences <- nass.spp.data[[i]]
+		presences <- presences[presences$HOW_MANY_ATLEAST!=0,]
+		presences <- merge(presences, coords.crosswalk)
+		# print(str(presences)); stop('cbw')
+		presences <- SpatialPointsDataFrame(coords=presences[,c('x.coord','y.coord')], data=data.frame(presences$HOW_MANY_ATLEAST), proj4string=unique.pts@proj4string, bbox=bbox(ref.raster))
+		# print(str(presences)); stop('cbw')
+		temp <- kd(presence.pts=presences, ref.raster=ref.raster, the.radius=the.radius, file.name=paste(output.path,spp.names[i],'.kd.',the.radius,'.tif',sep=''))
+		# stop('cbw')
+	}
 }
