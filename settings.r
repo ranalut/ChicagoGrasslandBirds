3c7f57e56e2811860f070614fd039861d3bb78c4
ranalut <chadwilsey@gmail.com>
2014-05-01 16:48:01 -0700

    update settings.r

--- settings.r
+++ settings.r
@@ -8,7 +8,7 @@ nass.path <- paste(drive,':/chicago_grasslands/gis/nass_layers/',sep='')
 landsat.path <- paste(drive,':/chicago_grasslands/landsat2/',sep='')
 output.path <- paste(drive,':/chicago_grasslands/models/',sep='')
 
-do.data.proc <- 'y' # See processing settings below.  Remove clouds (landsat.processing.r) before running.
+do.data.proc <- 'n' # See processing settings below.  Remove clouds (landsat.processing.r) before running.
 do.load.data <- 'n'
 do.spp.data <- 	'n'
 do.test.data <- 'n' # DO NOT OVERWRITE. Change output name below if turned on.
