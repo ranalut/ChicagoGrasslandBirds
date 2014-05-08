5439a9ca9c35ebbc571d8ee4fb01ea3357bbacbd
ranalut <chadwilsey@gmail.com>
2014-05-08 10:32:50 -0700

    update cv.rasters.r

--- cv.rasters.r
+++ cv.rasters.r
@@ -2,21 +2,25 @@
 # Coefficient of variation for each grid cell.
 library(raster)
 
-drive <- 'z'
+drive <- 'd'
 workspace <- paste(drive,':/chicago_grasslands/models/',sep='')
 ver <- 10
-spp.names <- c('boboli','sedwre','henspa','easmea','graspa')
+# spp.names <- c('boboli','sedwre','henspa','easmea','graspa')
+spp.names <- 'sedwre'
+startTime <- Sys.time()
 
 for (i in spp.names)
 {
 	nass.tif <- raster(paste(workspace,i,'.nass.v',ver,'.tif',sep=''))
 	landsat.tif <- raster(paste(workspace,i,'.landsat.v',ver,'.tif',sep=''))
-	cat('files loaded...')
+	cat('files loaded...',Sys.time()-startTime,'...')
 	landsat.tif <- projectRaster(from=landsat.tif, to=nass.tif, method="ngb")
-	cat('re-project complete...')
+	cat('re-project complete...',Sys.time()-startTime,'...')
 	temp.stack <- stack(nass.tif, landsat.tif)
 	cv.stack <- cv(temp.stack)
 	writeRaster(cv.stack,paste(workspace,i,'.cv.v',ver,'.tif',sep=''), overwrite=TRUE)
-	cat('file written\n')
-	stop('cbw')
+	cat('file written...',Sys.time()-startTime,'\n')
+	# stop('cbw')
 }
+
+# (CellStatistics(["boboli.nass.v10.tif", "boboli.landsat.v10.tif"], "STD", "NODATA") / CellStatistics(["boboli.nass.v10.tif", "boboli.landsat.v10.tif"], "MEAN", "NODATA")) * 100
