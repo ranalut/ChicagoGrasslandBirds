8c1c47bdac62ccd02576a4edc47d0ee774179c83
ranalut <chadwilsey@gmail.com>
2015-01-23 16:21:27 -0800

    update patch scripts

--- calc.patch.r
+++ calc.patch.r
@@ -15,10 +15,18 @@ calc.patch <- function(nass.map, class.vector, workspace, the.year, the.nass.var
 	# reassign
 	# will need to merge all rasters to get a single map again.
 	# writeRaster(focal.prop, paste(workspace,the.year,'_',the.nass.var,'_nass_clump_30m.tif',sep=''),overwrite=TRUE)
+	# return(patch)
+	freq.patch <- freq(patch, progress='text',useNA='no')
+	patch.size <- reclassify(patch, rcl=freq.patch)
+	writeRaster(patch.size, paste(workspace,the.year,'_',the.nass.var,'_nass_clump_30m.tif',sep=''),overwrite=TRUE)
+	# return(patch.size)
 }
 
 # # Single run
-workspace <- 'z:/chicago_grasslands/gis/nass_layers/'
-r.temp <- raster(paste(workspace,2010,'_nass_clip_30m.tif',sep=''))
-is.na(r.temp) <- 0 # Creates a temporary file
-calc.patch(nass.map=r.temp, class.vector=c(171,37,181), workspace=workspace, the.year=2010, the.nass.var='grass.hay')
+# workspace <- 'd:/chicago_grasslands/gis/nass_layers/'
+# r.temp <- raster(paste(workspace,2010,'_nass_clip_30m.tif',sep=''))
+# is.na(r.temp) <- 0 # Creates a temporary file
+# temp <- calc.patch(nass.map=r.temp, class.vector=c(171,37,181), workspace=workspace, the.year=2010, the.nass.var='grass.hay')
+# plot(temp)
+
+# output <- PatchStat(mat=r.temp, cellsize = 1, latlon = FALSE)
--- patch.size.r
+++ patch.size.r
@@ -30,32 +30,37 @@ cat('years:',years,'\n')
 startTime <- Sys.time()
 
 # stop('cbw')
-for (n in 1:length(radius))
+
+for (i in 1:length(years))
 {
-	for (i in 1:length(years))
+	r.temp <- raster(paste(workspace,years[i],'_nass_clip_30m.tif',sep=''))
+	# r.temp <- crop(x=r.temp,y=extent(600000,650000,2050000,2100000)) # For testing/debugging
+	# plot(r.temp)
+	is.na(r.temp) <- 0 # Creates a temporary file
+	
+	for (j in 1:length(nass.var))
 	{
-		r.temp <- raster(paste(workspace,years[i],'_nass_clip_30m.tif',sep=''))
-		# r.temp <- crop(x=r.temp,y=extent(600000,650000,2050000,2100000)) # For testing/debugging
-		# plot(r.temp)
-		is.na(r.temp) <- 0 # Creates a temporary file
-		
-		for (j in 1:length(nass.var))
+		if (file.exists(paste(workspace,years[i],'_',nass.var[j],'_nass_clump_30m.tif',sep=''))==TRUE) 
 		{
-			if (file.exists(paste(workspace,years[i],'_',nass.var[j],'_nass_30m_r',radius[n],'.tif',sep=''))==TRUE) 
-			{
-				cat('year',years[i],'variable',nass.var[j],Sys.time()-startTime,'\n')
-				next(j)
-			}
-			
-			calc.patch(nass.map=r.temp, class.vector=values[[j]], workspace=workspace, the.year=years[i], the.nass.var=nass.var[j])
 			cat('year',years[i],'variable',nass.var[j],Sys.time()-startTime,'\n')
-			# stop('cbw')
+			next(j)
 		}
 		
-		# Remove all the temporary files for that year's calculations.  
-		file.remove(dir('D:/Chicago_Grasslands/Temp/',full.names=TRUE))
-		# file.remove(dir('c:/users/cwilsey/appdata/local/temp/r_raster_cwilsey1',full.names=TRUE))
-		# file.remove(dir('c:/users/cwilsey/appdata/local/temp/r_raster_tmp/cwilsey',full.names=TRUE))
+		calc.patch(nass.map=r.temp, class.vector=values[[j]], workspace=workspace, the.year=years[i], the.nass.var=nass.var[j])
+		cat('year',years[i],'variable',nass.var[j],Sys.time()-startTime,'\n')
 		# stop('cbw')
 	}
+	
+	# Remove all the temporary files for that year's calculations.  
+	file.remove(dir('D:/Chicago_Grasslands/Temp/',full.names=TRUE))
+	# file.remove(dir('c:/users/cwilsey/appdata/local/temp/r_raster_cwilsey1',full.names=TRUE))
+	# file.remove(dir('c:/users/cwilsey/appdata/local/temp/r_raster_tmp/cwilsey',full.names=TRUE))
+	# stop('cbw')
+	
+	file.names <- as.list(paste(workspace,years[i],'_',nass.var,'_nass_clump_30m.tif',sep=''))
+	all.patch <- brick(file.names)
+	sum.patch <- calc(all.patch,fun=sum,na.rm=TRUE)
+	writeRaster(sum.patch, paste(workspace,years[i],'_nass_clump_30m.tif',sep=''),overwrite=TRUE)
 }
+
+
