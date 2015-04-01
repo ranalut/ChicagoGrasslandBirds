f0e0067bdf50758aad8777118b1b540fa1e83f80
ranalut <chadwilsey@gmail.com>
2015-03-31 22:08:03 -0700

    create pop.est.r

---
+++ pop.est.r
@@ -0,0 +1,44 @@
+
+library(raster)
+
+drive <- 'd' # 'z'
+ver <- 36
+
+spp.names <- c('boboli','sedwre','henspa','easmea','graspa')
+nass.path <- paste(drive,':/chicago_grasslands/models/v',ver,'/',sep='')
+
+output <- as.data.frame(matrix(NA,ncol=5,nrow=20))
+colnames(output) <- spp.names
+
+for (j in 1:20)
+{
+	for (i in 1:5)
+	{
+		# start <- Sys.time()
+		temp <- raster(paste(nass.path,spp.names[i],'.nass.v',ver,letters[j],'.tif',sep=''))
+		temp <- as.matrix(temp)
+		# Set a minimum cutoff below which we ignore the value assigned to the grid cell.
+		temp[temp < 0.15] <- NA
+		output[j,i] <- round(sum(temp,na.rm=TRUE) * 0.0509)
+		print(output)
+		# print(Sys.time()-start)
+		# stop('cbw')
+	}
+}
+
+write.csv(output,paste(nass.path,'pop.est.nass.v',ver,'.cut.0.15.csv',sep=''))
+
+load(file=paste(nass.path,'all.performance.v',ver,'.rdata',sep=''))
+library(Hmisc)
+output2 <- as.data.frame(matrix(NA,ncol=5,nrow=3))
+colnames(output2) <- spp.names
+rownames(output2) <- c('wt.mean','low','high')
+
+for (i in 1:5)
+{
+	weights <- test.eval[[i]]$cor
+	output2[1,i] <- round(wtd.mean(output[,i],weights=weights, na.rm=FALSE))
+	output2[2:3,i] <- round(range(output[,i]))
+}
+
+write.csv(output2,paste(nass.path,'median.pop.est.nass.v',ver,'.cut.0.15.csv',sep=''))
