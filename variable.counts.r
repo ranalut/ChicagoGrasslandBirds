85b3de688f800d582f98ab91e84b6362aca71071
ranalut <chadwilsey@gmail.com>
2016-08-11 15:33:35 -0700

    commit variable.counts.r

--- variable.counts.r
+++ variable.counts.r
@@ -8,14 +8,16 @@ nass.path <- paste(drive,':/chicago_grasslands/models/',sep='')
 
 load(paste(nass.path,"nass.species.models.v34bbf.rdata",sep=''))
 
-all.var <- NA
+all.var <- NULL
 
 for (i in 1:5)
 {
 	print(length(nass.models[[i]]$var.names))
 	all.var <- c(all.var,nass.models[[i]]$var.names)
+	png(paste(nass.path,spp.names[i],'.v34bbf.response.png',sep=''),width=960,height=1200)
+	    gbm.plot(nass.models[[i]],write.title = F,plot.layout=c(4,4),cex=2)
+	dev.off()
 }
 
-all.var <- all.var[-1]
 temp <- table(all.var)
 print(temp[order(temp,decreasing=TRUE)])
